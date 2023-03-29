e314d4a139aead77a13b84a85d3c1e6b9ac2f36c
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2023-03-29 14:39:11 +0100

    'bug' fixed in ex1

--- src/ex1a.ml
+++ src/ex1a.ml
@@ -6,36 +6,41 @@ let stdout, stderr =
 let file = try Unix.openfile Sys.argv.(1) [ Unix.O_RDONLY ] 0 with _ -> exit 1
 
 (* main program *)
-let rec file_manipulator buffer =
+let rec file_manipulator buffer fsize =
   (* Reads a line from the input *)
   let inn = read_line () |> String.split_on_char ' ' in
   match inn with
   | [ "+"; n ] ->
       Unix.lseek file (int_of_string n) Unix.SEEK_CUR |> ignore;
-      file_manipulator buffer
+      file_manipulator buffer fsize
   | [ "-"; n ] ->
       Unix.lseek file (int_of_string n * -1) Unix.SEEK_CUR |> ignore;
-      file_manipulator buffer
+      file_manipulator buffer fsize
   | [ "i"; n ] ->
       Unix.lseek file 0 Unix.SEEK_SET |> ignore;
       Unix.lseek file (int_of_string n) Unix.SEEK_CUR |> ignore;
-      file_manipulator buffer
+      file_manipulator buffer fsize
   | [ "f"; n ] ->
       (* Relative to the end | n + 1 because of EOF *)
       Unix.lseek file ((int_of_string n + 1) * -1) Unix.SEEK_END |> ignore;
-      file_manipulator buffer
+      file_manipulator buffer fsize
   | [ "r"; n ] ->
+      let n = int_of_string n in
+      let buffer = Bytes.sub buffer 0 fsize in
       let b = Bytes.create 101 in
-      let _ = Unix.read file b 0 (int_of_string n) in
+      let nn = Unix.read file b 0 n in
+      let b = Bytes.sub b 0 nn in
       let buffer = Bytes.concat Bytes.empty [ buffer; b ] in
-      file_manipulator buffer
-  | [ "l"; "0" ] -> file_manipulator (Bytes.map (fun _ -> Char.chr 0) buffer)
+      file_manipulator buffer (fsize + nn)
+  | [ "l"; "0" ] -> file_manipulator (Bytes.map (fun _ -> Char.chr 0) buffer) 0
   | [ "s"; "0" ] ->
-      let buffer = Bytes.concat Bytes.empty [ buffer; String.to_bytes "\n" ] in
-      Unix.write stdout buffer 0 (Bytes.length buffer) |> ignore
+      (* Add '\n' *)
+      let fbuffer = Bytes.concat Bytes.empty [ buffer; String.to_bytes "\n" ] in
+      (* write the fbuffer to the output *)
+      Unix.write stdout fbuffer 0 (Bytes.length fbuffer) |> ignore
   | _ ->
       let errmsg = String.to_bytes "Falta de argumentos\n" in
       Unix.write stderr errmsg 0 (Bytes.length errmsg) |> ignore;
-      file_manipulator buffer
+      file_manipulator buffer fsize
 
-let () = file_manipulator (Bytes.create 101)
+let () = file_manipulator (Bytes.create 101) 0
