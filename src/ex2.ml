0da35133029ca1d07eb4a9c25c52d500402ee34e
zazedd <leomendesantos@gmail.com>
2023-05-14 23:06:07 +0100

    theres still a loop

--- src/ex2.ml
+++ src/ex2.ml
@@ -10,6 +10,7 @@ let max = Sys.argv.(4) |> int_of_string
 let pid_father = getpid ()
 
 let read_block rd buf len =
+  sleep 2;
   let n = read rd buf 0 len in
   match n with 0 -> None | b -> Some b
 
@@ -24,21 +25,29 @@ let rec make_pipes = function
       openfile str [ O_RDWR ] 0 :: make_pipes (m - 1)
 
 let rec read_write_loop rd wr buf mm =
-  sleep 3;
   let n = read_block rd buf (Bytes.length buf) in
   match n with
   | None -> read_write_loop rd wr buf mm
   | Some b -> (
       let buf = Bytes.sub buf 0 b in
       let s = buf |> Bytes.to_string in
-      Format.printf "[mm:%d:%s]\n@." mm s;
-      let token = (s |> int_of_string) + 1 in
-      let token_bytes = token |> string_of_int |> Bytes.of_string in
-      match token > max with
-      | true -> exit 0
-      | false ->
-          write wr token_bytes 0 b |> ignore;
-          read_write_loop rd wr (Bytes.create 1024) mm)
+      match s with
+      | "e" ->
+          print_endline "Got exit message, sending exit message, leaving@.";
+          write_substring wr "e" 0 b |> ignore;
+          exit 0 (* Exit because exit message recieved*)
+      | _ -> (
+          Format.printf "[mm:%d:%s]\n@." mm s;
+          let token = (s |> int_of_string) + 1 in
+          let token_bytes = token |> string_of_int |> Bytes.of_string in
+          match token > max with
+          | true ->
+              print_endline "End reached, sending error message, leaving@.";
+              write_substring wr "e" 0 b |> ignore;
+              exit 0 (* Send exit message and exit process *)
+          | false ->
+              write wr token_bytes 0 b |> ignore;
+              read_write_loop rd wr (Bytes.create 1024) mm))
 
 let rec make_processes pipes n = function
   | i when i = n -> ()
@@ -54,8 +63,7 @@ let rec make_processes pipes n = function
           read_write_loop rd wr buf 1
       | _ ->
           (* Parent process *)
-          (* let first_pipe = List.nth pipes 0 in *)
-          print_endline "yo we just started";
+          print_endline "yo we just started@.";
           make_processes pipes n 1)
   | i -> (
       let pid = fork () in
@@ -69,50 +77,14 @@ let rec make_processes pipes n = function
           read_write_loop rd wr buf i
       | _ ->
           (* Parent process *)
-          (* let first_pipe = List.nth pipes 0 in *)
-          (* let buf = Bytes.of_string "0" in *)
-          (* write stdout buf 0 (Bytes.length buf) |> ignore; *)
-          Format.printf "made process %d" i;
+          Format.printf "made process %d\n@." i;
           make_processes pipes n (i + 1))
 
 let pipes = make_pipes n
 let () = make_processes pipes n 0
 
 let () =
-  if pid_father = getpid () then
+  if pid_father = getpid () then (
     let buf = Bytes.of_string "0" in
-    write (List.hd pipes) buf 0 (Bytes.length buf) |> ignore
-
-(* let rec make_processes pipes n = function *)
-(*   | 0 -> () *)
-(*   | 1 -> ( *)
-(*       let pid = fork () in *)
-(*       match pid with *)
-(*       | 0 -> *)
-(*           (* Child process *) *)
-(*           Format.printf "[m:%d]\n@." 1; *)
-(*           let rd = List.nth pipes (n - 1) in *)
-(*           let wr = List.nth pipes 0 in *)
-(*           let buf = Bytes.create 1024 in *)
-(*           read_write_loop rd wr buf 1 *)
-(*       | _ -> *)
-(*           (* Parent process *) *)
-(*           (* let first_pipe = List.nth pipes 0 in *) *)
-(*           print_endline "yo i reached the end of this shit"; *)
-(*           make_processes pipes 0) *)
-(*   | m -> ( *)
-(*       let pid = fork () in *)
-(*       match pid with *)
-(*       | 0 -> *)
-(*           (* Child process *) *)
-(*           Format.printf "[m:%d]\n@." m; *)
-(*           let rd = List.nth pipes (m - 2) in *)
-(*           let wr = List.nth pipes (m - 1) in *)
-(*           let buf = Bytes.create 1024 in *)
-(*           read_write_loop rd wr buf m *)
-(*       | _ -> *)
-(*           (* Parent process *) *)
-(*           (* let first_pipe = List.nth pipes 0 in *) *)
-(*           (* let buf = Bytes.of_string "0" in *) *)
-(*           (* write stdout buf 0 (Bytes.length buf) |> ignore; *) *)
-(*           make_processes pipes (m - 1)) *)
+    write (List.hd pipes) buf 0 (Bytes.length buf) |> ignore;
+    wait () |> ignore)
