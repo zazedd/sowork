a15cc6b5ff8d20c5fef5466c69a246da2902f2e3
ProgramingIsTheFuture <franciscosantosfcp@hotmail.com>
2023-05-15 23:00:31 +0100

    Version 1 is finished

--- src/ex2.ml
+++ src/ex2.ml
@@ -3,9 +3,8 @@ open Unix
 let () = Printexc.record_backtrace true
 let perm = 0o640
 let n = Sys.argv.(1) |> int_of_string
-
-(* let p = Sys.argv.(2) *)
-(* let t = Sys.argv.(3) *)
+let p = Sys.argv.(2) |> float_of_string
+let t = Sys.argv.(3) |> float_of_string
 let max = Sys.argv.(4) |> int_of_string
 let pid_father = getpid ()
 
@@ -20,7 +19,6 @@ let rec make_pipes = function
       openfile str [ O_RDWR ] 0 :: make_pipes (m - 1)
 
 let read_block rd buf len =
-  sleep 2;
   let n = read rd buf 0 len in
   match n with 0 -> None | b -> Some b
 
@@ -34,19 +32,29 @@ let rec read_write_loop rd wr buf mm =
       match s with
       | "e" ->
           Format.printf "Got exit message, sending exit message, leaving@.";
-          write_substring wr "e" 0 b |> ignore;
+          let s = "e" |> Bytes.of_string in
+          write wr s 0 (Bytes.length s) |> ignore;
           exit 0 (* Exit because exit message recieved*)
       | _ -> (
-          Format.printf "[mm:%d:%s]\n@." mm s;
+          let () = Random.self_init () in
+          let rand_num = Random.float 1.0 in
+          let () =
+            if rand_num < p then
+              let () = Format.printf "Blocking process %d\n@." mm in
+              sleepf t
+            else ()
+          in
+          Format.printf "[Process:%d;Value:%s]\n@." mm s;
           let token = (s |> int_of_string) + 1 in
           let token_bytes = token |> string_of_int |> Bytes.of_string in
           match token > max with
           | true ->
               Format.printf "End reached, sending exit message, leaving@.";
-              write_substring wr "e" 0 b |> ignore;
+              let s = "e" |> Bytes.of_string in
+              write wr s 0 (Bytes.length s) |> ignore;
               exit 0 (* Send exit message and exit process *)
           | false ->
-              write wr token_bytes 0 b |> ignore;
+              write wr token_bytes 0 (Bytes.length token_bytes) |> ignore;
               read_write_loop rd wr (Bytes.create 1024) mm))
 
 let rec make_processes pipes n = function
@@ -56,35 +64,30 @@ let rec make_processes pipes n = function
       match pid with
       | 0 ->
           (* Child process *)
-          Format.printf "[m:%d]\n@." 0;
           let rd = List.nth pipes (n - 1) in
           let wr = List.nth pipes 0 in
           let buf = Bytes.create 1024 in
           read_write_loop rd wr buf 1
       | _ ->
           (* Parent process *)
-          Format.printf "yo we just started@.";
           make_processes pipes n 1)
   | i -> (
       let pid = fork () in
       match pid with
       | 0 ->
           (* Child process *)
-          Format.printf "[m:%d]\n@." i;
           let rd = List.nth pipes (i - 1) in
           let wr = List.nth pipes i in
           let buf = Bytes.create 1024 in
-          read_write_loop rd wr buf i
+          read_write_loop rd wr buf (i + 1)
       | _ ->
           (* Parent process *)
-          Format.printf "made process %d\n@." i;
           make_processes pipes n (i + 1))
-
-let pipes = make_pipes n
-let () = make_processes pipes n 0
 
 let () =
   if pid_father = getpid () then (
-    let buf = Bytes.of_string "0" in
+    let pipes = make_pipes n in
+    let () = make_processes pipes n 0 in
+    let buf = Bytes.of_string "1" in
     write (List.hd pipes) buf 0 (Bytes.length buf) |> ignore;
     wait () |> ignore)
